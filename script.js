2d874c6785cd98765f6407ba042079ab9f0a03e0
HARIHARAN4369 <108862738+HARIHARAN4369@users.noreply.github.com>
2022-09-22 22:14:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-var res=fetch('https://restcountries.com/v2/all');
-//console.log(res);
-res.then((data)=>(data.json()) .then((data1)=>foo(data1)));
-function foo(arr){
-    for(var i=0;i<=arr.length;i++){
-    var div=document.createElement("div")
+var res=fetch("https://data.covid19india.org/v4/min/data.min.json");
+// res.then((data) => (data.json()).then((data1)=>console.log(data1)));
+res.then((data) => data.json())
+.then((data1)=>foo(data1));
+function foo(data1){
+    for(var i in data1){
+    // console.log(i,data1[i].total.confirmed)
+    // }
+    var div=document.createElement("div");
     div.innerHTML=`<div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">${arr[i].name}</h5>
+    <h5 class="card-title">${"covid 19 state details"}</h5> 
+    <h5 class="card-title">${[i]}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${data1[i].total.confirmed}</h6>
       
     </div>
-  </div>`
+  </div>`;
     document.body.append(div);
-
 }
-} 
+}
